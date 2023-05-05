535d25b258017ac3a3afab37c29325385eee1a65
ABAYO-WEB <132690555+ABAYO-WEB@users.noreply.github.com>
2023-05-05 17:49:34 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link  href="aos/aos.css" rel="stylesheet">
+
+</head>
+<body>
+	
+<ul>
+	<li><a href="#home">home</a></li>
+	<li><a href="#about"> About</a></li>
+	<li><a href="#price"> services</a></li>
+	<li><a href="#contact"> contact</a></li>
+</ul>
+
+<section class="home" id="home" >
+	<div class="overlay">
+	<h2 data-aos="fade-left" data-aos-delay="200"><u> Explore all beautifull</u> <span>interial design</span></h2>
+</div>
+</section>
+<section class="about" id="about">
+	<center>
+		<h3>About us </h3>
+	</center>
+
+	<div class="p1">	
+	<p>Improved hub a company specialized in interial design </p>
+	<p>with our expert in design we offer different services usch as:</p> 
+	<ul>
+		<li>interial home design</li>
+		<li> Site decoration</li>
+		<li>wedding deco</li>
+		<li>3D design</li>
+		<li>wood design</li>
+	</ul>
+	</div>
+	<img src="images/photo.jpg">
+	
+</section>
+
+<section class="products"data-aos="zoom-in" data-aos-delay="500" id="products">
+	<center>
+<h3> Products</h3> 
+</center>
+
+
+	</div>
+	<div class="card"style="padding: 10px;
+	float: left;">
+	
+		<img src="images/photo1.jpg">
+
+		<div class="text">
+			<h3> Interial design</h3>
+			<p> allspecial wood design</p>
+			<p>COST</p>
+			<P> 35$/day</P>
+		</div>
+
+	</div>
+		<div class="card"style="padding: 10px;
+	float: left;">
+	
+		<img src="images/photo1.jpg">
+
+		<div class="text">
+			<h3> Interial design</h3>
+			<p> allspecial wood design</p>
+			<p>COST</p>
+			<P> 35$/day</P>
+		</div>
+
+	</div>
+		<div class="card"style="padding: 10px;
+	float: left;">
+	
+		<img src="images/photo1.jpg">
+
+		<div class="text">
+			<h3> Interial design</h3>
+			<p> allspecial wood design</p>
+			<p>COST</p>
+			<P> 35$/day</P>
+		</div>
+
+	</div>	<div class="card"style="padding: 10px;
+	float: left;">
+	
+		<img src="images/photo1.jpg">
+
+		<div class="text">
+			<h3> Interial design</h3>
+			<p> allspecial wood design</p>
+			<p>COST</p>
+			<P> 35$/day</P>
+		</div>
+
+	</div>
+</section>
+<section class="contact" id="contact">
+	<center>
+	<h3>Contact us</h3>
+</center>
+<div class="box">
+	<div class="container">
+	<div class="location">
+		<h4>Location</h4>
+		<p>Kigali city</p>
+		<P>Gasabo</P>
+			<h4>Email</h4>
+		<p>mueric@gmail.com</p>
+			<h4>call</h4>
+		<p>078885663844</p>
+
+	</div>
+	
+</div>
+	<div class="form">
+		<form>
+			<label>Your Name</label> 
+					<input type="text" name=""placeholder="Names">
+					<h4></h4>
+		<input type="text" name="" placeholder="" style="margin-left: 20px;">
+		</form>
+
+	</div>
+</div>
+
+	
+</section>
+
+<script src="aos/aos.js"></script>
+<script src="main.js"></script>
+</body>
+</html>
